0482089067900a41e6b1aa7b4ceec838a890458b
Wiggidy <rlr0017@auburn.edu>
2023-03-15 20:07:12 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/WiggidyW/WETC_parser
+
+go 1.20
+
+require github.com/evepraisal/go-evepraisal v0.0.0-20230218071758-d9f2139d5843 // indirect
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"github.com/evepraisal/go-evepraisal/parsers"
+	"encoding/json"
+	"fmt"
+	"os"
+	"io"
+)
+
+type OutItem struct {
+	Name	string	`json:"name"`
+	Quantity	int64	`json:"quantity"`
+}
+
+func main() {
+	itemsMap := make(map[string]int64)
+	var items = make([]OutItem, 0)
+
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	result, _ := parsers.AllParser(parsers.StringToInput(string(input)))
+
+	for _, sub_result := range result.(*parsers.MultiParserResult).Results {
+		switch r := sub_result.(type) {
+			default:
+				fmt.Println("Unreachable")
+			case *parsers.AssetList:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.CargoScan:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.Contract:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.EFT:
+				itemsMap[r.Ship] += 1
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.Fitting:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.Industry:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.Listing:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.LootHistory:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.PI:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.SurveyScan:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.ViewContents:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.MiningLedger:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.MoonLedger:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.HeuristicResult:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.DScan:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += 1
+				}
+			case *parsers.Compare:
+				for _, item := range r.Items {
+					itemsMap[item.Name] += 1
+				}
+			case *parsers.Wallet:
+				for _, item := range r.ItemizedTransactions {
+					itemsMap[item.Name] += item.Quantity
+				}
+			case *parsers.Killmail:
+				for _, item := range r.Dropped {
+					itemsMap[item.Name] += item.Quantity
+				}
+				for _, item := range r.Destroyed {
+					itemsMap[item.Name] += item.Quantity
+				}
+		}
+	}
+
+	for name, quantity := range itemsMap {
+		if quantity > 0 {
+			items = append(items, OutItem{Name: name, Quantity: quantity})
+		}
+	}
+
+	if err := json.NewEncoder(os.Stdout).Encode(items); err != nil {
+		panic(err)
+	}
+}
